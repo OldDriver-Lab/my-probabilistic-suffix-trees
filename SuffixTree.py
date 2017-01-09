52697a0720c2474263972f7e348b5699b0bec35d
lunatictwo@163.com <lunatictwo@163.com>
2017-01-09 19:48:24 +0800

    添加Node分支量count，方便概率计算

--- SuffixTree.py
+++ SuffixTree.py
@@ -9,6 +9,8 @@ class TreeNode(object):
         self.count = 1  # 统计此结点代表的字符串出现的次数
         self.name = 'root'  # 标记node的名称
         self.children = {}
+        self.totalchild = 0  # 统计node子节点个数
+        self.probability = {}
 
 
 class Tree(object):
@@ -18,6 +20,7 @@ class Tree(object):
     def add(self, sequence):
         print("------------------ ", sequence, " ------------------")
         node = self.root
+        node.totalchild += 1
         for c in sequence:
             print(c)
             print("node.name:", node.name)
@@ -25,15 +28,16 @@ class Tree(object):
                 child = TreeNode()
                 node.children[c] = child
                 node = child
+                node.totalchild += 1
                 node.name = c + str(node.count)
             else:
                 node = node.children[c]
+                node.totalchild += 1
                 node.count = node.count + 1
                 print("node.count:", node.count)
 
     def count_seq(self, sequence):
-        '''计算序列出现的次数
-        '''
+        '''计算序列出现的次数'''
         node = self.root
         for c in sequence:
             if c not in node.children:
@@ -49,7 +53,8 @@ def gen_tree(input_file, output_file):
     with open(input_file) as f:
         for line in f:
             # 增加'$'用来区别是否是完整后缀
-            line = line.strip() + '$'
+            # line = line.strip() + '$'
+            line = line.strip()
             for i in range(len(line)):
                 l = line[i:]
                 tree.add(l)
@@ -65,6 +70,19 @@ def gen_tree(input_file, output_file):
             str_list.remove()
         print(str_list)
 
+    node = tree.root
+    print('root count-----:', node.totalchild)
+    for c in str_list:
+        print(c)
+        if c not in node.children:
+            node.probability[c] = 0
+        else:
+            # todo :增加概率计算
+            pass
+
+    print(node.children.keys())
+    # 此处已经计算出单个Node的数据量(即分支量,概率计算的被除数)
+    print(node.children['A'].children['N'].totalchild)
     # with open(output_file, 'wb') as f:
     # pickle.dump(tree, f)
 
